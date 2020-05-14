19f59aa8d938773ca74d35011a421650fd325f07
tknightz <hatuyen2699@gmail.com>
2020-05-14 21:05:04 +0700

    Remove cache and local trash.

--- _site/assets/js/search-data.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Tìm kiếm mù BFS (Breath First Search) và UCS (Uniform-cost Search)",
-        "excerpt":"Trong môn “Nhập môn trí tuệ nhân tạo” thì tìm kiếm là chương đầu tiên ta được học, nhưng ko chỉ riêng Trí tuệ nhân tạo mà hầu như tìm kiếm (search) có mặt ở khắp nơi trong ngành CNTT. Nhằm tìm hiểu sâu hơn về thuật toán, code,.. và...","categories": ["study"],
-        "tags": ["study","college"],
-        "url": "http://localhost:4000/study/2020/05/13/study-ai.html"
-      },]
